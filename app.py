afb56eff82555c3f8e43d8838e538edce4f2b66a
josue-org-pe <99492161+josue-org-pe@users.noreply.github.com>
2024-08-10 19:28:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import cv2
-import pytesseract
 from PIL import Image
 import numpy as np
+from google.cloud import vision
 
-# RUTA DE TESSERACT 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\yolor\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+# Inicializar cliente de Google Cloud Vision
+client = vision.ImageAnnotatorClient()
 
 st.markdown("<h1 style='text-align: center;'>ESCÁNER DE DOCUMENTOS</h1>", unsafe_allow_html=True)
 
 # MODO DE SUBIR LA FOTO 
-col1,col2,col3  =st.columns([2,3,4])
+col1, col2, col3 = st.columns([2, 3, 4])
 col1.subheader("1. MODO")
 with col2:
     modo = st.selectbox("", ("SUBE TU IMAGEN", "USAR CAMARA"))
@@ -22,18 +22,22 @@ with col3:
     elif modo == "USAR CAMARA":
         picture = st.camera_input("Toma una foto")
 image = None
-if imagen is not None :
+if imagen is not None:
     image = Image.open(imagen)
 elif picture is not None:
     image = Image.open(picture)
 if image is not None:
     if st.button("ESCANEAR"):
         st.image(image, caption="", use_column_width=True)
-        image_np = np.array(image)
-        gray_image = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
-        _, thresh_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY)
-        text = pytesseract.image_to_string(thresh_image)
-        st.subheader("2. TEXTO :")
-        st.code(text)
+        # Convertir la imagen a bytes para enviar a Google Cloud Vision
+        img_byte_arr = cv2.imencode('.jpg', np.array(image))[1].tobytes()
+        image = vision.Image(content=img_byte_arr)
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+        if texts:
+            st.subheader("2. TEXTO :")
+            st.code(texts[0].description)
+        else:
+            st.warning("No se detectó texto en la imagen.")
 else:
     st.warning("Por favor, sube una imagen o toma una foto para continuar.")
